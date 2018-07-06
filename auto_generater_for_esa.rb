09c2379571c1435ca4a73536f0714d890bb15908
fakestarbaby <fakestarbaby@gmail.com>
2018-07-06 15:04:29 +0900

    :sparkles: Send massage to slack with esa post url

--- auto_generater_for_esa.rb
+++ auto_generater_for_esa.rb
@@ -1,7 +1,10 @@
 require "active_support/time"
 require "esa"
+require "slack-notifier"
 
 client = Esa::Client.new(access_token: ENV["ESA_ACCESS_TOKEN"], current_team: ENV["ESA_TEAM"])
 posts = client.posts(q: "in:#{ENV["SEARCH_QUERY_CATEGORY"]} title:#{ENV["SEARCH_QUERY_TITLE"]} created:>#{Time.current.prev_week.strftime("%Y-%m-%d")}")
 parent_post_number = posts.body["posts"].first["number"]
-client.create_post(parent_post_id: parent_post_number)
+post = client.create_post(parent_post_id: parent_post_number)
+
+Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL']).ping("#{ENV["SLACK_MESSAGE_TEMPLATE"]} #{post.body["url"]}")
